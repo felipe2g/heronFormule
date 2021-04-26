259d78353d62ab660cbd04ce57d179e10d27b811
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2021-04-26 00:51:47 -0300

    make calc for second triangle

--- src/heronFormule/Main.java
+++ src/heronFormule/Main.java
@@ -19,7 +19,23 @@ public class Main {
 		pX = (xa + xb + xc) / 2;
 		areaX = Math.sqrt(pX * (pX - xa) * (pX - xb) * (pX - xc));
 		
+		
+		System.out.println("Enter the measures of triangle Y: ");
+		ya = sc.nextDouble();
+		yb = sc.nextDouble();
+		yc = sc.nextDouble();
+
+		pY = (ya + yb + yc) / 2;
+		areaY = Math.sqrt(pY * (pY - ya) * (pY - yb) * (pY - yc));
+		
 		System.out.printf("Triangle X area: %.4f\n", areaX);
+		System.out.printf("Triangle Y area: %.4f\n", areaY);
+		
+		if(areaX > areaY) {
+			System.out.print("Larger area: X");
+		} else {
+			System.out.print("Larger area: Y");
+		}
 	}
 
 }
